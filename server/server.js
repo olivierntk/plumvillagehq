ac10ac9f2f5d39e4198b655e557b76632559de61
olivier nguyen <olivierntk@gmail.com>
2016-01-01 13:37:18 -0800

    added google analytics

--- server/server.js
+++ server/server.js
@@ -68,6 +68,16 @@ function renderFullPage(html, initialState) {
         <title>Plumvillage HQ</title>
         <meta name="description" content="An site to gather Plum Village's online resources" />
         <meta name="keywords" content="PlumVillage, Dharma talks, Peace, Joy, Dharma, Sangha, Thich Nhat Hanh, Deer Park monastery, Blue Cliff monastery" />
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-71915923-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
       </head>
       <body>
         <div id="app">${html}</div>
